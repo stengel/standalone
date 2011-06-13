5b98dfdd226cc6d14c82ca42e5d6f0484c4c5594
Karen Bletzer <kbletzer@gmail.com>
2011-06-13 12:32:05 -0700

    added test file, updated normalFormToLaTeX.java

---
+++ lse/standalone/normalFormToLaTeX_README.txt
@@ -0,0 +1,34 @@
+The class normalFormToLaTeX will create a formatted LaTeX document from a matrix or pair of matrices representing game payoffs. 
+
+There are three possible ways to provide information to the class.  The first is to use the class as a standalone executable and pass a filename containing the matrix information.  The information should be formatted as follows:
+
+1 2 3
+2 2 1
+
+4 2 1
+3 3 2  
+
+The size of the matrices will be auto-detected. Use of one matrix will format the output as single payoff mode.
+
+The second way of invoking the class is via another java class, where the filename is passed in to the class constructor.  See standaloneClassTester.java for an example.
+
+The third way of invoking the class is via another java class, where the information is passed as arrays of Strings.  See standaloneClassTester.java for an example.
+
+***Setup 
+
+compile normalFormToLaTeX.java as follows:
+
+>>  javac normalFormToLaTeX.java
+
+
+***Usage examples for first method of using class
+call the class on a matrix file:
+
+>> java normalFormToLaTeX matrix22.txt
+
+a .tex file will be produced with the name matrix22.txt.tex
+
+OR
+
+please see the contents of standaloneClassTester.java for more details on how to format payoff matrices passed in via parameter.
+
---
+++ lse/standalone/standaloneClassTester.java
@@ -0,0 +1,56 @@
+package lse.standalone;
+
+public class standaloneClassTester 
+{
+	public static void main (String [] args)
+	{	
+		String[] filename = { "matrix22.txt", "matrix23.txt", "matrix34.txt", "matrix33.txt", "matrix33single.txt" };
+		
+		for (int i = 0; i < filename.length; i++)
+		{
+			normalFormToLaTeX ml = new normalFormToLaTeX(filename[i]);
+			ml.createLaTeXFile(); 
+		} 
+		
+		String[][] p1_22 = {{"2", "3"}, {"1", "1/2"}};
+		String[][] p1_23 = {{"2", "3", "0"}, {"1", "1/2", "-2/3"}};
+		String[][] p1_33 = {{"2", "3", "1"}, {"9/3", "-3", "5"},{"1", "1/2", "2"}};
+		String[][] p1_34 = {{"2", "3", "1", "4"}, {"9/3", "-3", "5", "2"},{"1", "1/2", "2", "-3/4"}};
+		String[][] p1_44 = {{"2", "3", "1", "4"}, {"9/3", "-3", "5", "2"},{"1", "1/2", "2", "-3/4"},{"1", "3", "2", "4"}};
+		
+		String[][] p2_22 = {{"1", "3"}, {"1", "1/2"}};
+		String[][] p2_23 = {{"1", "3", "-11"}, {"-1", "1/2", "2/3"}};
+		String[][] p2_33 = {{"1", "3", "1"}, {"9/3", "-3", "4"},{"-1", "1/2", "2"}};
+		String[][] p2_34 = {{"1", "3", "5", "4"}, {"9/3", "-3", "-5", "2"},{"1", "-1/2", "2", "-3/4"}};
+		String[][] p2_44 = {{"1", "3", "5", "4"}, {"9/3", "-3", "-5", "2"},{"1", "-1/2", "2", "-3/4"},{"4", "1/2", "2", "3"}};
+		
+		//no fraction formatting & display best response
+		normalFormToLaTeX ml22 = new normalFormToLaTeX(p1_22, p2_22, false, true); 
+		//nice fraction formatting & display best response
+		normalFormToLaTeX ml23 = new normalFormToLaTeX(p1_23, p2_23, true, true);
+		//nice fraction formatting, don't  display best response
+		normalFormToLaTeX ml33 = new normalFormToLaTeX(p1_33, p2_33, true, false);
+		normalFormToLaTeX ml34 = new normalFormToLaTeX(p1_34, p2_34); 
+		normalFormToLaTeX ml44 = new normalFormToLaTeX(p1_44, p2_44, false, false); 
+		
+		normalFormToLaTeX ml22single = new normalFormToLaTeX(p1_22, null, false, true); 
+		//nice fraction formatting & display best response
+		normalFormToLaTeX ml23single = new normalFormToLaTeX(p1_23, null, true, true);
+		//nice fraction formatting, don't  display best response
+		normalFormToLaTeX ml33single = new normalFormToLaTeX(p1_33, null, true, false);
+		normalFormToLaTeX ml34single = new normalFormToLaTeX(p1_34, null); 
+		normalFormToLaTeX ml44single = new normalFormToLaTeX(p1_44, null, false, false); 
+		
+		ml22.createLaTeXFile("s22.tex");
+		ml23.createLaTeXFile("s23.tex");
+		ml33.createLaTeXFile("s33.tex");
+		ml34.createLaTeXFile("s34.tex");
+		ml44.createLaTeXFile("s44.tex");
+		
+		ml22single.createLaTeXFile("s22single.tex");
+		ml23single.createLaTeXFile("s23single.tex");
+		ml33single.createLaTeXFile("s33single.tex");
+		ml34single.createLaTeXFile("s34single.tex");
+		ml44single.createLaTeXFile("s44single.tex");
+	}
+}
