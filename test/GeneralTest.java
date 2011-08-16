64d5d46642b8933bff0633d48c9db72767748ee7
Karen Bletzer <kbletzer@gmail.com>
2011-08-15 17:18:42 -0700

    removed un-needed parameter from constructor of XMLToLaTeX class

--- test/GeneralTest.java
+++ test/GeneralTest.java
@@ -1,13 +1,27 @@
 package lse.standalone;
-/* last updated August 13, 2011 */
+/* author: K. Bletzer */
+/* last updated August 15, 2011 */
 import java.io.File;
 
+/*
+ * This class covers general testing of the six conversion classes.  
+ * It assumes that each set of "starter" test files (for nfg, efg, and strategic
+ * flat file conversion) are in a defined directory.  Any number of test files can 
+ * be used.
+ * 
+ * The methods included in this class will allow the tester to exercise a simple
+ * conversion, or a "round trip" conversion (e.g., efg to xml and back to efg).
+ * 
+ * The validation of the results of the conversion are done manually outside of this
+ * class using other tools.
+ */
 public class GeneralTest 
 {
-	private String nfgFilePath = "<< fill in path to nfgToXML_test_files >>";
-	private String efgFilePath = "<< fill in path to efgToXML_test_files >>";
-	private String latexFilePath = "<< fill in path to strategicToXML_test_files >>";
+	private String nfgFilePath = "< path to nfg files here >";
+	private String efgFilePath = "< path to efg files here >";
+	private String latexFilePath = "< path to strategic flat files here >";
 	
+	/* convert all of the .nfg files in the nfg folder from nfg format to xml */
 	public void allNFGToXML()
 	{
 		File folder = new File(this.nfgFilePath);
@@ -30,6 +44,9 @@ public class GeneralTest
 	    }
 	}
 	
+	/* convert all of the .xml files in the nfg folder from xml to nfg.
+	 * Since there are two types of nfg format this method will convert to 
+	 * both differentiating the output based on the file name. */
 	public void allXMLToNFG()
 	{
 		File folder = new File(this.nfgFilePath);
@@ -44,12 +61,12 @@ public class GeneralTest
 	    	  {
 	    		  System.out.println(listOfFiles[i].toString());
 	    		  XMLToNFG xtn = new XMLToNFG(filename);
-	    		  
-	    		  xtn.setFileSuffix("_out.nfg");
+	    		   
+	    		  xtn.setFileSuffix("_out.nfg");  //format 1: outcome
 	    		  xtn.setNFGFormat("outcome");
 	    		  xtn.convertXMLToNFG();
 	    		  
-	    		  xtn.setFileSuffix("_pay.nfg");
+	    		  xtn.setFileSuffix("_pay.nfg");  //format 2: payoff
 	    		  xtn.setNFGFormat("payoff");
 	    		  xtn.convertXMLToNFG();
 	    	  }
@@ -57,6 +74,7 @@ public class GeneralTest
 	    }
 	}
 	
+	/* convert all of the .xml files in the efg folder from xml to efg */
 	public void allXMLToEFG()
 	{
 		File folder = new File(this.efgFilePath);
@@ -78,6 +96,7 @@ public class GeneralTest
 	    } 
 	}
 	
+	/* convert all of the .efg files in the efg folder from efg to xml */
 	public void allEFGToXML()
 	{
 		File folder = new File(this.efgFilePath);
@@ -100,6 +119,7 @@ public class GeneralTest
 	    }
 	}
 	
+	/* convert all of the strategic flat files in the flat file folder to xml */
 	public void allStrategicFilesToXML()
 	{
 		File folder = new File(this.latexFilePath);
@@ -122,6 +142,7 @@ public class GeneralTest
 	    }
 	}
 	
+	/* convert all xml files in the strategic flat file folder to LaTeX format */
 	public void allXMLToLaTeX()
 	{
 		File folder = new File(this.latexFilePath);
@@ -136,26 +157,28 @@ public class GeneralTest
 	    	  if (filename.endsWith(".xml"))
 	    	  {
 	    		  System.out.println(listOfFiles[i].toString());
-	    		  String[] s = {};
-	    		  XMLToLaTeX x = new XMLToLaTeX(filename, s);
+	    		  XMLToLaTeX x = new XMLToLaTeX(filename);
 	    		  x.convertXMLToLaTeX(); 
 	    	  }
 	      }
 	    }
 	}
 	
+	/* nfg to xml and back to _nfg.nfg */
 	public void roundTripNFG()
 	{
 		this.allNFGToXML();
 	    this.allXMLToNFG();
 	}
 	
+	/* efg to xml and back to _efg.efg */
 	public void roundTripEFG()
 	{
 		this.allEFGToXML();
 		this.allXMLToEFG();
 	}
 	
+	/* flat file to xml to .tex */
 	public void flatToLaTeX()
 	{
 		this.allStrategicFilesToXML();
